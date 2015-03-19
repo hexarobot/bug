06f6d65c806be1e0b435bd2e4da3f8031afc722b
Sergei Podust <sergei.podust@tieto.com>
2015-03-19 18:37:31 +0200

    up down

--- UNO_20_Servos_Controller/UNO_20_Servos_Controller.ino
+++ UNO_20_Servos_Controller/UNO_20_Servos_Controller.ino
@@ -131,6 +131,12 @@ static char SerialCharToSend[50] = ".detratS slennahC 81 ovreSDH";
 static int SerialNbOfCharToSend = 0;  //0= none, 1 = [0], 2 = [1] and so on...
 
 static long servoAngles[18] = {90,130,30,   90,130,30,   90,130,30,   90,50,150,   90,50,150,   90,50,150};
+
+static long leg_set_1[3] = {1,7,13};
+static long leg_set_1_dir[3] = {1,1,-1};
+static long leg_set_2[3] = {4,10,16};
+static long leg_set_2_dir[3] = {-1,-1,1};
+
 static int dir = 1;
 
 void ServoMove(int Channel, long PulseHD, long SpeedHD, long Time)
@@ -160,6 +166,10 @@ void setup()
   #if HDServoMode == 18
     TIMSK0 = 0;                       // Disable timer 0. This can reduse jitter some more. But it's used for delay() funtions.
   #endif                              // This will disable delay()!
+  for(int i = 0; i < 18 ; i++) 
+  { 
+    ServoMoveAngle(i, servoAngles[i], 500);
+  }
 }
 
 void loop()
@@ -168,10 +178,6 @@ void loop()
     if(Serial.available() > 0) 
     {
       UpDown(Serial.read());
-    }
-    for(int i = 0; i < 18 ; i++) 
-    { 
-      ServoMoveAngle(i, servoAngles[i], 100);
     }
   #elif HDServoMode == 20
     // for testing
@@ -188,21 +194,13 @@ void UpDown(char input)
   {
     dir = -1;
   } 
-  else if(input == 'z') 
+  else if(input == 'c') 
   {
-    servoAngles[1] = servoAngles[1] - dir*5;
-    servoAngles[4] = servoAngles[4] - dir*5;
-    servoAngles[7] = servoAngles[7] - dir*5;
-    servoAngles[10] = servoAngles[10] + dir*5;
-    servoAngles[13] = servoAngles[13] + dir*5;
-    servoAngles[16] = servoAngles[16] + dir*5;
-
-    servoAngles[2] = servoAngles[2] + dir*5;
-    servoAngles[5] = servoAngles[5] + dir*5;
-    servoAngles[8] = servoAngles[8] + dir*5;
-    servoAngles[11] = servoAngles[11] - dir*5;
-    servoAngles[14] = servoAngles[14] - dir*5;
-    servoAngles[17] = servoAngles[17] - dir*5;
+    set1_vertical();
+  } 
+  else if(input == 'c') 
+  {
+    set2_vertical();
   } 
   else if(input == 'x') 
   {
@@ -292,6 +290,24 @@ void UpDown(char input)
   {
     servoAngles[17] = servoAngles[17] + dir;
   }
+}
+
+void set1_vertical()
+{
+    for(int i = 0; i < 3 ; i++)
+    {
+      servoAngles[leg_set_1[i]] = servoAngles[leg_set_1[i]] + leg_set_1_dir[i]*dir*30;
+      ServoMoveAngle(leg_set_1[i], servoAngles[leg_set_1[i]], 500);
+    }
+}
+
+void set2_vertical()
+{
+    for(int i = 0; i < 3 ; i++)
+    {
+      servoAngles[leg_set_2[i]] = servoAngles[leg_set_2[i]] + leg_set_2_dir[i]*dir*30;
+      ServoMoveAngle(leg_set_2[i], servoAngles[leg_set_2[i]], 500);
+    }
 }
 
 long CheckRange(long PulseHDValue)
